4c89bdd892ec74aef13ed655120a6550d393da05
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2023-05-03 17:37:59 -0400

    feat: validation

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,9 @@
 class Venue < ApplicationRecord
+  has_many :events
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :capacity, presence: true
+  validates :capacity, numericality: { only_integer: true }
+  validates :capacity, numericality: { greater_than: 0 }
 end
